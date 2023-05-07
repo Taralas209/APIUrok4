c7ee1b29f637384d8b8cdd62647a4c178be85dd1
Taralas209 <alexej.zikunow@gmail.com>
2023-05-07 11:38:05 +0200

    publish text to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,12 @@
+import os
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ['TELEGRAM_API_KEY']
+bot = telegram.Bot(token=api_key)
+
+channel_id = -1001968826354
+bot.send_message(text="Привет, канал!", chat_id=channel_id)
+
