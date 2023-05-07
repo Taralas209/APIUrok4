50ef1c06265a9154f9977713d4fad7193d6d9b2d
Taralas209 <alexej.zikunow@gmail.com>
2023-05-07 11:53:58 +0200

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,7 @@ load_dotenv()
 api_key = os.environ['TELEGRAM_API_KEY']
 bot = telegram.Bot(token=api_key)
 
-channel_id = -1001968826354
-bot.send_message(text="Привет, канал!", chat_id=channel_id)
+channel_id = '-1001968826354'
+#bot.send_message(text="Привет, канал!", chat_id=channel_id)
+bot.send_document(chat_id=channel_id, document=open('images/spacex_0.png', 'rb'))
 
