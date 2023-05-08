978059ebae76d24ec417cf81db62409317f8c3a3
Taralas209 <alexej.zikunow@gmail.com>
2023-05-08 12:47:09 +0200

    Added a feature to the "telegram-bot.py" script: post a random image or one selected by the user

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,30 @@
 import os
+import random
 import telegram
+import argparse
 from dotenv import load_dotenv
 
-load_dotenv()
+def publish_image(api_key, channel_id, folder_name, image_name):
+    if image_name is None:
+        folder_path, folder_names, file_names = next(os.walk(folder_name))
+        image_to_publish = random.choice(file_names)
+    else:
+        image_to_publish = image_name
 
-api_key = os.environ['TELEGRAM_API_KEY']
-bot = telegram.Bot(token=api_key)
+    full_path_to_image = os.path.join(folder_name, image_to_publish)
+    bot = telegram.Bot(token=api_key)
+    with open(full_path_to_image, 'rb') as image_file:
+        bot.send_document(chat_id=channel_id, document=image_file)
 
-channel_id = '-1001968826354'
-#bot.send_message(text="Привет, канал!", chat_id=channel_id)
-bot.send_document(chat_id=channel_id, document=open('images/spacex_0.png', 'rb'))
 
+if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.environ['TELEGRAM_API_KEY']
+    channel_id = '-1001968826354'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_name', help='Name of the folder containing images', default='images')
+    parser.add_argument('--image_name', help='Name of the image to publish')
+    args = parser.parse_args()
+
+    publish_image(api_key, channel_id, args.folder_name, args.image_name)
