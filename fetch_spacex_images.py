dc1b71c7d1a60356305e76d72f102960da6de584
Taralas209 <alexej.zikunow@gmail.com>
2023-05-16 14:20:01 +0200

    I added a default value to the "launch_id" argument and removed "if/else" from the function (fetch_spacex_images)

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,17 +2,11 @@ import argparse
 import requests
 from common_scripts import get_file_extension, create_folder
 
-def fetch_spacex_images(folder_path, launch_id=None):
-    if launch_id is None:
-        spacex_url = 'https://api.spacexdata.com/v5/launches/latest'
-        response = requests.get(spacex_url)
-        response.raise_for_status()
-        images = [response.json()['links']['patch']['small']]
-    else:
-        spacex_url = 'https://api.spacexdata.com/v5/launches/{}'.format(launch_id)
-        response = requests.get(spacex_url)
-        response.raise_for_status()
-        images = response.json()['links']['flickr']['original']
+def fetch_spacex_images(folder_path, launch_id):
+    spacex_url = 'https://api.spacexdata.com/v5/launches/{}'.format(launch_id)
+    response = requests.get(spacex_url)
+    response.raise_for_status()
+    images = response.json()['links']['flickr']['original']
 
     for i, image_url in enumerate(images):
         file_extension = get_file_extension(image_url)
@@ -23,7 +17,7 @@ def fetch_spacex_images(folder_path, launch_id=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--launch_id', help='ID of the SpaceX launch.')
+    parser.add_argument('--launch_id', default="latest", help='ID of the SpaceX launch.')
     args = parser.parse_args()
 
     folder_name = "images"
