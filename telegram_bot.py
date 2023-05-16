1b807232bd8b6a162374c13774413a6ace7e9c2e
Taralas209 <alexej.zikunow@gmail.com>
2023-05-16 12:45:14 +0200

    Moved "if/else" out of the "def publish_image()" function

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,17 +5,14 @@ import argparse
 from dotenv import load_dotenv
 
 def publish_image(api_key, channel_id, folder_name, image_name):
-    if image_name is None:
-        folder_path, folder_names, file_names = next(os.walk(folder_name))
-        image_to_publish = random.choice(file_names)
-    else:
-        image_to_publish = image_name
-
-    full_path_to_image = os.path.join(folder_name, image_to_publish)
+    full_path_to_image = os.path.join(folder_name, image_name)
     bot = telegram.Bot(token=api_key)
     with open(full_path_to_image, 'rb') as image_file:
         bot.send_document(chat_id=channel_id, document=image_file)
 
+def get_random_image_name(folder_name):
+    folder_path, folder_names, file_names = next(os.walk(folder_name))
+    return random.choice(file_names)
 
 if __name__ == "__main__":
     load_dotenv()
@@ -26,5 +23,8 @@ if __name__ == "__main__":
     parser.add_argument('--folder_name', help='Name of the folder containing images', default='images')
     parser.add_argument('--image_name', help='Name of the image to publish')
     args = parser.parse_args()
+
+    if args.image_name is None:
+        args.image_name = get_random_image_name(args.folder_name)
 
     publish_image(api_key, channel_id, args.folder_name, args.image_name)
