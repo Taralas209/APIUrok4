94e8771c78f376d09f0daba1913c40ca2aebaba5
Taralas209 <alexej.zikunow@gmail.com>
2023-05-18 15:56:55 +0200

    Small Update for common_scripts.py

--- common_scripts.py
+++ common_scripts.py
@@ -3,10 +3,9 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 def create_folder(folder_name):
-    folder_path = os.path.join(os.getcwd(), folder_name)
-    os.makedirs(folder_path, exist_ok=True)
+    folder_path = Path(folder_name)
+    folder_path.mkdir(parents=True, exist_ok=True)
     print(f"Папка '{folder_name}' успешно создана.")
-
     return folder_path
 
 def get_file_extension(image_url):
